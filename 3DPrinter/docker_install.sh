ad175b34887e8b8f95929f5120a6f9274c59c4c2
Magform <nicolas25.02.2003.nf@gmail.com>
2024-06-20 15:40:33 +0200

    add mqtt broker

---
+++ 3DPrinter/docker_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+
+# Install necessary dependencies
+sudo apt-get install -y ca-certificates curl
+
+# Create directory for Docker keyrings
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# Download Docker GPG key
+sudo curl -fsSL https://download.docker.com/linux/raspbian/gpg -o /etc/apt/keyrings/docker.asc
+
+# Set appropriate permissions for the Docker GPG key
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add Docker repository to package sources
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/raspbian \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package lists again to include Docker repository
+sudo apt-get update
+
+# Install Docker packages
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+
+# Inform user about the completion of the installation
+echo "Docker installation completed successfully."
